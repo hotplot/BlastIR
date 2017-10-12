c5f198466142a7754dc7b45c0e236cfe88e4056e
Sam Birch <mail@worldofsam.com>
2017-10-12 21:20:11 +1300

    Removed flashing heartbeat indicator

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,6 @@
 #include "mqtt.h"
 #include "wifi.h"
 
-#define HB_LED_PIN (2)
-
-static void heartbeat()
-{
-    // Turn on the LED
-    digitalWrite(HB_LED_PIN, LOW);
-
-    // Short flashes indicate we are running fine
-    // Long flashes indicate an error
-    if (wifi::connected && mqtt::connected)
-        delay(250);
-    else
-        delay(1000);
-    
-    // Turn off the LED
-    digitalWrite(HB_LED_PIN, HIGH);
-}
-
 void setup()
 {
     Serial.begin(115200);
@@ -36,18 +18,11 @@ void setup()
     // Establish a WiFi connection
     // A WiFi connection callback is responsible for establishing a MQTT connection once we have an IP
     wifi::setup();
-
-    // Setup heartbeat LED pin
-    pinMode(HB_LED_PIN, OUTPUT);
-    digitalWrite(HB_LED_PIN, HIGH);
 }
 
 void loop()
 {
-    unsigned long startTime = millis();
-    heartbeat();
-    unsigned long duration = millis() - startTime;
-    delay(2000 - duration);
+    delay(500);
 
     if (ir::hasRecordedData) {
         ir::processRecordedData();
